0ad4fce8e9c81da7ab57bbd6ee84b086334c4d63
Aboudou Rachidi <aboud976@live.fr>
2016-09-30 15:34:58 +0200

    Formulaire modifiction compte

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,4 +1,6 @@
 <?php
+use micro\orm\DAO;
+use micro\utils\RequestUtils;
 /**
  * Gestion des users
  * @author jcheron
@@ -19,6 +21,35 @@ class Users extends \_DefaultController {
 		$this->loadView("user/vAdd.html",array("user"=>$user,"disabled"=>$disabled));
 	}
 
+	public function profil(){
+		$user = DAO::getOne("Utilisateur", "id='".Auth::getUser()->getId()."'");
+		$this->loadView("user/vProfil.html",array("user"=>$user));
+	}
+	
+	/**
+	 * Met à jour à partir d'un post une instance de $className<br>
+	 * L'affectation des membres de l'objet par le contenu du POST se fait par appel de la méthode setValuesToObject()
+	 * @see _DefaultController::setValuesToObject()
+	 */
+	public function updateProfil(){
+		$user = DAO::getOne("Utilisateur", "id='".Auth::getUser()->getId()."'");
+		if(RequestUtils::isPost()){
+			$className=$this->model;
+			$object=new $className();
+			$this->setValuesToObject($object);
+			if($_POST["id"]){
+				try{
+					DAO::update($object);
+					$msg= $this->messageSuccess("<b>Vos informations ont été mis à jour</b>",5000,true);
+					$this->onUpdate($object);
+					$this->profil();
+				}catch(\Exception $e){
+					$msg=new DisplayedMessage("Impossible de modifier l'instance de ".$this->model,"danger");
+				}
+			}
+		}
+	}
+	
 	/* (non-PHPdoc)
 	 * @see _DefaultController::setValuesToObject()
 	 */
