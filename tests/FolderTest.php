9f1e0d6d8e731fd62d9850706a3ea765f4c9f988
Aboudou Rachidi <aboud976@live.fr>
2016-10-18 10:31:05 +0200

    PHPunit

---
+++ tests/FolderTest.php
@@ -0,0 +1,40 @@
+<?php
+use micro\orm\DAO;
+
+class FolderTest extends \PHPUnit_Framework_TestCase {
+	private $config;
+	public function setUp(){
+		global $config;
+		$this->config = $config;
+		DAO::connect($config["database"]["dbName"]);
+	}
+	
+	public function testConfigIsOk(){
+		$this->assertArrayHasKey("siteUrl", $this->config);
+	}
+	
+	public function testBaseDirectoryExists(){
+		$cloud = $this->config["cloud"];
+		$this->assertTrue(file_exists("./../".$cloud["root"]));
+	}
+	
+	public function testCreateFolder(){
+		$cloud = $this->config["cloud"];
+		$pathname="./../".$cloud["root"]."/".$cloud["prefix"]."test";
+		$this->assertFileNotExists($pathname);
+		DirectoryUtils::mkDir($pathname);
+		$this->assertFileExists($pathname);
+		rmdir($pathname);
+	}
+	
+	public function testExistUsersFolder(){
+		$cloud = $this->config["cloud"];
+		$users = DAO::getAll("utilisateur");
+		foreach ($users as $user){
+			$pathname="./../".$cloud["root"]."/".$cloud["prefix"].$user->getLogin();
+			$this->assertFileExists($pathname);
+			//DirectoryUtils::mkDir($pathname);
+		}
+		
+	}
+}
